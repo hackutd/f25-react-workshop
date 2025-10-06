ecaccc1598bfd9c72d5a619534d2c4e3891b9b7f
Veer <veermickey@gmail.com>
2025-10-06 18:40:51 -0500

    removed MovieCard, favorites page, simplified home

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,17 +1,8 @@
 "use client";
-import MovieCard from "./components/MovieCard";
-import {useState, useEffect} from "react";
-
-type Movie = {
-  id: number;
-  title: string;
-  poster_path?: string;
-  release_date?: string;
-};
 
 
 export default function Home() {
-  const movies: Movie[] = [
+  const movies = [
     {
       id: 1,
       title: "Inception",
@@ -34,10 +25,14 @@ export default function Home() {
 
   return (
     <div>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 m-8">
+      <div>
         {movies.map((movie) => (
-            <MovieCard movie={movie} key={movie.id} />
-          ))}
+          <div key={movie.id} className="m-4">
+            <p>{movie.title}</p>
+            <p>{movie.poster_path}</p>
+            <p>{movie.release_date}</p>
+          </div> ))
+        }
       </div>
     </div>
   );
